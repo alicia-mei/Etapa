f3900ae37042f3a15630b78f8fc2b56163fc4cf4
alicia-mei <117045780+alicia-mei@users.noreply.github.com>
2025-04-12 21:16:16 -0300

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,10 +1,10 @@
-import cv2
+import cv2 #openCV lib
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0) #opens camera
 
-while cv2.waitKey(1) == -1:
-    status, imagem = camera.read()
-    cv2.imshow("alguma coisa ai", imagem)
+while cv2.waitKey(1) == -1: #loop finishes when a key is pressed
+    status, imagem = camera.read() #captures image
+    cv2.imshow("image", imagem) #shows captured image 
     print(status)
-camera.release()
-cv2.destroyAllWindows()
+camera.release() #frees camera
+cv2.destroyAllWindows() #closes the window
