ff90f9615a8f0dd7e0cb9aa925f085da0d0f0ba9
alicia-mei <117045780+alicia-mei@users.noreply.github.com>
2025-04-12 21:08:20 -0300

    Upload webcam.py
    Uses OpenCV library

---
+++ webcam.py
@@ -0,0 +1,10 @@
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+while cv2.waitKey(1) == -1:
+    status, imagem = camera.read()
+    cv2.imshow("alguma coisa ai", imagem)
+    print(status)
+camera.release()
+cv2.destroyAllWindows()
